6d45a960661184f1e7dedbc34eb0ce17473f0a68
Kaoru Fukumoto <kaoru@threelm.com>
2012-06-28 15:06:06 -0700

    more cleanups

--- MobileConsole/gen/com/threelm/kaoru/lab/mc/R.java
+++ MobileConsole/gen/com/threelm/kaoru/lab/mc/R.java
@@ -14,20 +14,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int add_user=0x7f070007;
-        public static final int editText1=0x7f070002;
-        public static final int editText2=0x7f070004;
-        public static final int editText3=0x7f070006;
-        public static final int gridLayout1=0x7f070000;
-        public static final int setting=0x7f070008;
-        public static final int textView1=0x7f070001;
-        public static final int textView2=0x7f070003;
-        public static final int textView3=0x7f070005;
+        public static final int add_user=0x7f070000;
+        public static final int setting=0x7f070001;
     }
     public static final class layout {
-        public static final int add_user=0x7f030000;
-        public static final int admin_user_list=0x7f030001;
-        public static final int list_item=0x7f030002;
+        public static final int admin_user_list=0x7f030000;
+        public static final int list_item=0x7f030001;
     }
     public static final class menu {
         public static final int menu=0x7f060000;
