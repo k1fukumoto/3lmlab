f90120944ce6f88b36f12de4493654608d814c6f
Kaoru Fukumoto <kaoru@threelm.com>
2012-07-06 15:51:16 +0900

    Cleanup python implementation

--- api/EnterpriseServer.rb
+++ api/EnterpriseServer.rb
@@ -4,6 +4,8 @@ require 'json'
 require 'pp'
 
 class EnterpriseServer
+  attr_reader :token,:expiration
+
   def initialize
     @host = 'docomo-jp.3lm.com'
     @port = 8443
@@ -13,6 +15,7 @@ class EnterpriseServer
 
     resp = self.getSessionToken :username=> @user, :password => @pass
     @token = resp['sessionToken']
+    @expiration = resp['expiration'].to_i / 1000
   end
 
   def method_missing(m,*args)
--- api/apitest.py
+++ api/apitest.py
@@ -6,27 +6,30 @@ import urllib2
 import time
 
 class EnterpriseServer:
-    def __init__(self,cmd):
+    def __init__(self):
         self.host = 'docomo-jp.3lm.com'
         self.port = 8443
         self.user = 'superadmin@docomo.3lm.com'
         self.pswd = 'bf1C2p54o72VY8P8LSOV'
         self.base_url = "https://%s:%s/api/mt/" % (self.host,self.port)
-        
+        self.token = ''
         resp = self.post('getSessionToken',{'username': self.user, 'password': self.pswd})
         self.token = resp['sessionToken']
         
     def post(self,cmd,jd):
+        if self.token:
+            jd['sessionToken'] = self.token
         data = urllib.urlencode({'json':json.dumps(jd)})
         return json.loads(''.join(urllib2.urlopen(self.base_url+cmd ,data)))
 
-enterprises = URL('searchForEnterprise').post({
-        'sessionToken': token
-        'enterprise': {}
-    })
+es = EnterpriseServer()
+for ent in (es.post('searchForEnterprise', {'enterprise': {}}))['enterprises']:
+    for user in (es.post('getAllAcquirements',{'enterprise': {'domain': ent['domain']}}))['users']:
+        for device in user['devices']:
+            if 'mac' not in device: device['mac'] = ' '
+            if 'imei' not in device: device['imei'] = ' '
+            p = '*' if device['provisioned'] else ' '
+            print "%-16s%-32s %s %-18s %-18s" % (ent['name'], user['email'],p,device['mac'], device['imei'])
 
-print enterprises
 
-# for e in enterprises:
-#    print '%s (%s) %d' % (e['name'],e['domain'],e['maxActivations'])
 
